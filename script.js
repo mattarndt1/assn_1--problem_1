7d1ffd64490dbeca4d380c1f2c98f093cbfaa538
mattarndt1 <60207898+mattarndt1@users.noreply.github.com>
2021-02-06 22:51:36 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 var mymap = L.map('mapid').setView([-94, 34], 13);
 
-L.tileLayer('https://{s}.tile.thunderforest.com/pioneer/{z}/{x}/{y}.png?apikey={apikey}', {
-	attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	apikey: '<your apikey>',
-	maxZoom: 22
+var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	subdomains: 'abcd',
+	minZoom: 1,
+	maxZoom: 16,
+	ext: 'jpg'
 }).addTo(mymap);
 
 var marker = L.marker([51.5, -0.09]).addTo(mymap);
