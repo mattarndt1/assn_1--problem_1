0767e47d0729915c62f4a25b84ff82154eb92549
mattarndt1 <60207898+mattarndt1@users.noreply.github.com>
2021-02-06 23:20:57 -0600

    Update script.js

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ var Stamen_Toner = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/
 	ext: 'png'
 }).addTo(mymap);
 
-var marker1 = L.marker([38.6057018,-90.4263799]).addTo(mymap);
-var marker2 = L.marker([38.6727119,-90.5671538]).addTo(mymap);
-var marker3 = L.marker([38.5922542,-90.4881896]).addTo(mymap);
-var marker4 = L.marker([38.5922542,-90.4881896]).addTo(mymap);
-var marker5 = L.marker([38.5922542,-90.4881896]).addTo(mymap);
+var marker1 = L.marker([38.63562, -90.23385]).addTo(mymap);
+var marker2 = L.marker([38.67270, -90.43588]).addTo(mymap);
+var marker3 = L.marker([38.65043, -90.33838]).addTo(mymap);
+var marker4 = L.marker([38.62638, -90.33460]).addTo(mymap);
+var marker5 = L.marker([38.56254, -90.40731]).addTo(mymap);
 
 
 marker1.bindPopup("Chipotle - Grand Blvd STL");
