e347a339c2ea4d1a4c0430073e72ad9dd5cddf87
mattarndt1 <60207898+mattarndt1@users.noreply.github.com>
2021-02-06 22:56:57 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
-L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
+var Stamen_TerrainLabels = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain-labels/{z}/{x}/{y}{r}.{ext}', {
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	subdomains: 'abcd',
-	minZoom: 1,
-	maxZoom: 16,
-	ext: 'jpg'
+	minZoom: 0,
+	maxZoom: 18,
+	ext: 'png'
 }).addTo(mymap);
 
 var marker = L.marker([51.5, -0.09]).addTo(mymap);
