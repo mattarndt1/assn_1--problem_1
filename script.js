1d80d877cc28d6a5e63def6a756d607e107c179c
mattarndt1 <60207898+mattarndt1@users.noreply.github.com>
2021-02-06 22:58:47 -0600

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain-background/{z}/{x
 	minZoom: 0,
 	maxZoom: 18,
 	ext: 'png'
-})
+}).addTo(mymap);
+
 L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain-labels/{z}/{x}/{y}{r}.{ext}', {
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	subdomains: 'abcd',
