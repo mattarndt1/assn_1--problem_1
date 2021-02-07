d61fc125ee3fbd9cdac78c2e270099db53d47ff5
mattarndt1 <60207898+mattarndt1@users.noreply.github.com>
2021-02-06 23:12:51 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var mymap = L.map('mapid').setView([38.5, -90.3], 13);
+var mymap = L.map('mapid').setView([38.5, -90.3], 7);
 
 var Stamen_Toner = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}', {
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
@@ -8,18 +8,18 @@ var Stamen_Toner = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/
 	ext: 'png'
 }).addTo(mymap);
 
-var marker1 = L.marker([38.63660, -90.20268]).addTo(mymap);
-var marker2 = L.marker([38.65540, -90.32204]).addTo(mymap);
-var marker3 = L.marker([38.67721, -90.40416]).addTo(mymap);
-var marker4 = L.marker([38.46775, -90.39442]).addTo(mymap);
-var marker5 = L.marker([38.63226, -90.35887]).addTo(mymap);
+var marker1 = L.marker([38.6057018,-90.4263799]).addTo(mymap);
+var marker2 = L.marker([38.6727119,-90.5671538]).addTo(mymap);
+var marker3 = L.marker([38.5922542,-90.4881896]).addTo(mymap);
+var marker4 = L.marker([38.5922542,-90.4881896]).addTo(mymap);
+var marker5 = L.marker([38.5922542,-90.4881896]).addTo(mymap);
 
 
-marker1.bindPopup("Chipotle #1");
-marker2.bindPopup("Chipotle #2");
-marker3.bindPopup("Chipotle #3");
-marker4.bindPopup("Chipotle #4");
-marker5.bindPopup("Chipotle #5");
+marker1.bindPopup("Chipotle - Grand Blvd STL");
+marker2.bindPopup("Chipotle - Creve Coeur");
+marker3.bindPopup("Chipotle - Clayton");
+marker4.bindPopup("Chipotle - Hanley Rd STL");
+marker5.bindPopup("Chipotle - Kirkwood");
 
 var popup = L.popup();
 
